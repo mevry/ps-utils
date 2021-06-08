d0ee8b4b72889f5d7af5b62439c1177f333357ab
Derek Miller <derekjmiller@gmail.com>
2021-06-08 12:35:40 -0700

    add AD Replication subnet parse

--- AdOneLiners.ps1
+++ AdOneLiners.ps1
@@ -1,2 +1,5 @@
 #Get enabled AD Computers, sorted by LastLogonDate
 Get-AdComputer -Filter * -Properties OperatingSystem,OperatingSystemVersion,LastLogonDate,Enabled | ? Enabled -eq $true | sort-object -property LastLogonDate -Descending | Select Name,OperatingSystem,OperatingSystemVersion,LastLogonDate
+
+#Get AD Replication subnets
+Get-ADReplicationSubnet -Filter * | select Name,@{n="Site";e={$_.Site.Split(",") | select -first 1 | % {$_.Substring(3)}}}
